7108e4586dccb96312c36637bd0d7655251fe890
asyed-25 <71899114+asyed-25@users.noreply.github.com>
2020-10-02 15:28:13 -0500

    Session 2 Assignments

---
+++ Ch.1 P4.py
@@ -0,0 +1,10 @@
+# Declare variable for how many gallons the user puts in
+# Have the user input a gallon amount
+usergal = int(input())
+
+# Declare variable for how many gallons are in a liter
+# 
+liters = usergal * 3.785
+
+# Display the output of how many liters there are in x amount of gallons of gas
+print(usergal * 3.785)
