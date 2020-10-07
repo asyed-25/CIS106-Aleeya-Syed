59f776849234421206f5c2c1b1e71b2398aa280e
asyed-25 <71899114+asyed-25@users.noreply.github.com>
2020-10-07 15:38:16 -0500

    Add files via upload

---
+++ Session 3 Assignments/Area, Perimeter.py
@@ -0,0 +1,11 @@
+# Declare variables for the width and height of the rectangle.
+width = int(input())
+height = int(input())
+
+# Define how to calculate area and perimeter
+area = width * height
+perimeter = 2 * (width + height)
+
+# Display the area and perimeter
+print(area)
+print(perimeter)
